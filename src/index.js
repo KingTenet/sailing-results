d99d0da2a3d2385d374700c0a9a9b3b9cbb1fbff
Felix Morley <felix.morley@gmail.com>
2022-03-31 10:03:27 +0100

    Added pwa support for offline app use

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import * as serviceWorker from './serviceWorkerRegistration';
+serviceWorker.register();
+
 
 const rootElement = document.getElementById("root");
 
