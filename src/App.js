066677661640e196efce87c98173fd266098a09d
Felix Morley <felix.morley@gmail.com>
2022-06-14 10:38:21 +0100

    Optionall bootstrap location history for hierarchical navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
-import { Link, useRoutes, Navigate } from "react-router-dom";
-// import { BrowserRouter as Router } from "react-router-dom";
+import { useRoutes, Navigate, useLocation, matchRoutes, Outlet } from "react-router-dom";
 import { HashRouter as Router } from "react-router-dom";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useAppState } from "./useAppState";
 
@@ -20,6 +18,7 @@ import { useServices } from "./useAppState";
 import RegisterAnotherButtons from "./components/RegisterAnotherButtons";
 
 const ADD_ANOTHER_HELM_WORKFLOW = true;
+const BOOTSTRAP_HISTORY = false;
 
 function Home() {
     const [appState] = useAppState();
@@ -31,58 +30,146 @@ function Home() {
         if (appState) {
             console.log(services.readOnly);
             if (!services.readOnly) {
-                navigate("races/");
+                navigate("races");
             }
             else {
-                navigate("series/");
+                navigate("series");
             }
         }
     }, [appState]);
 
-    if (!appState || !services) {
-        return (
-            <>
-            </>
-        )
-    }
-
-
     return (
-        <>
-            <Flex direction="row" width="100vh" justify={"space-between"}>
-                <Text>Home</Text>
-            </Flex>
-            <Box>
-                <Link to="races/">Update Races</Link>
-            </Box>
-        </>
+        <></>
     )
 }
 
+function NavWrapper() {
+    return <Outlet />
+}
+
+const ROOT_PATHNAME = "/";
+const ROUTES = [
+    {
+        path: ROOT_PATHNAME, element: <StateWrapper />, children: [
+            { path: ROOT_PATHNAME, element: <Home />, index: true },
+            { path: '/races/', element: <Races liveOnly={true} /> },
+            { path: '/races/:raceDate/:raceNumber', element: <Race backButtonText="Back to races" /> },
+            { path: '/races/:raceDate/:raceNumber/register', element: <RegisterHelm addAnotherHelmWorkflow={ADD_ANOTHER_HELM_WORKFLOW} /> },
+            { path: '/races/:raceDate/:raceNumber/registerAnother/:registered', element: <RegisteredHelm><RegisterAnotherButtons /></RegisteredHelm> },
+            { path: '/races/:raceDate/:raceNumber/ood', element: <RegisterOOD /> },
+            { path: '/races/:raceDate/:raceNumber/fleetFinish/:registered', element: <RegisteredHelm><FinishHelm /></RegisteredHelm> },
+            { path: '/races/:raceDate/:raceNumber/pursuitFinish/:registered', element: <RegisteredHelm><FinishHelm /></RegisteredHelm> },
+            {
+                path: '/series/', element: <NavWrapper />, children: [
+                    { path: '/series/', element: <Series />, index: true },
+                    {
+                        path: '/series/:season/:series', element: <NavWrapper />, children: [
+                            { path: '/series/:season/:series', element: <SeriesPoints />, index: true },
+                            { path: '/series/:season/:series/:raceDate/:raceNumber', element: <Race backButtonText="Back to series" /> },
+                        ]
+                    }
+                ]
+            },
+
+        ]
+    },
+    { path: '/*', element: <Navigate to="/" /> },
+];
+
 function MyRoutes() {
-    let element = useRoutes([
-        {
-            path: '/', element: <StateWrapper />, children: [
-                { path: '/', element: <Home />, index: true },
-                { path: '/races/', element: <Races liveOnly={true} /> },
-                { path: '/races/:raceDate/:raceNumber', element: <Race backButtonText="Back to races" /> },
-                { path: '/races/:raceDate/:raceNumber/register', element: <RegisterHelm addAnotherHelmWorkflow={ADD_ANOTHER_HELM_WORKFLOW} /> },
-                { path: '/races/:raceDate/:raceNumber/registerAnother/:registered', element: <RegisteredHelm><RegisterAnotherButtons /></RegisteredHelm> },
-                { path: '/races/:raceDate/:raceNumber/ood', element: <RegisterOOD /> },
-                { path: '/races/:raceDate/:raceNumber/fleetFinish/:registered', element: <RegisteredHelm><FinishHelm /></RegisteredHelm> },
-                { path: '/races/:raceDate/:raceNumber/pursuitFinish/:registered', element: <RegisteredHelm><FinishHelm /></RegisteredHelm> },
-                { path: '/series/', element: <Series /> },
-                { path: '/series/:season/:series', element: <SeriesPoints /> },
-                { path: '/series/:season/:series/:raceDate/:raceNumber', element: <Race backButtonText="Back to series" /> },
-            ]
-        },
-        { path: '/*', element: <Navigate to="/races" /> },
-    ]);
+    let element = useRoutes(ROUTES);
 
     return element
 }
 
+/**
+ * This component pushes each required URL into the history stack to allow navigation to be hierarchical.
+ * State must be passed in from outside Router due to re-mounting on location updates
+ */
+function NavigationWrapper({ navStack, updateNavStack, updateTargetPath, targetPath, isReady, updateIsReady }) {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isReady) {
+            return;
+        }
+
+        if (!targetPath) {
+            updateTargetPath(location);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (isReady || !targetPath) {
+            return;
+        }
+        if (navStack === undefined) {
+            const targetMatches = matchRoutes(ROUTES, location);
+            const targetMatchParents = targetMatches && targetMatches
+                .filter((match) => match?.route?.children)
+                .filter((match) => match.pathnameBase !== targetPath.pathname);
+            if (targetMatchParents.length) {
+                updateNavStack(targetMatchParents.reverse());
+            }
+            else {
+                updateIsReady(true);
+            }
+        }
+    }, [targetPath]);
+
+    useEffect(() => {
+        if (isReady) {
+            return;
+        }
+        if (navStack && navStack.length && navStack.at(-1).pathnameBase === location.pathname) {
+            updateNavStack((prev) => {
+                const newNavStack = [...prev].slice(0, -1);
+                console.log(newNavStack);
+                return newNavStack;
+            });
+        }
+        else if (targetPath && navStack && !navStack.length && targetPath.pathname === location.pathname) {
+            updateIsReady(true);
+        }
+    }, [location]);
+
+    useEffect(() => {
+        if (isReady) {
+            return;
+        }
+        if (navStack) {
+            if (navStack.length) {
+                const nextLocation = [...navStack].pop();
+                navigate(nextLocation, { replace: nextLocation.pathname === ROOT_PATHNAME });
+            }
+            else {
+                navigate(targetPath);
+            }
+        }
+    }, [navStack]);
+
+    if (!isReady) {
+        return <></>
+    }
+
+    return <MyRoutes />
+}
+
+
 function App() {
+    const [navStack, updateNavStack] = useState();
+    const [targetPath, updateTargetPath] = useState();
+    const [isReady, updateIsReady] = useState(false);
+
+    if (BOOTSTRAP_HISTORY) {
+        return (
+            <Router>
+                <NavigationWrapper navStack={navStack} updateNavStack={updateNavStack} targetPath={targetPath} updateTargetPath={updateTargetPath} isReady={isReady} updateIsReady={updateIsReady} />
+            </Router>
+        )
+    }
+
     return (
         <Router>
             <MyRoutes />
