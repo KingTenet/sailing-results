fd789353455fd7ca3161177e76e4e147005ab803
Felix Morley <felix.morley@gmail.com>
2024-11-06 18:43:31 +0000

    disable sw

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,23 @@
-import { ConfigEnv, defineConfig, loadEnv, UserConfig, UserConfigExport } from 'vite'
+import { defineConfig, loadEnv, UserConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import react from '@vitejs/plugin-react'
-import process from 'node:process'
-import VitePWAPlugin from "./sw.vite.config"
-
+import react from "@vitejs/plugin-react";
+import process from "node:process";
 
 export default defineConfig(({ mode }: UserConfig) => {
-    // Load env file based on `mode` in the current working directory.
-    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-    const env = loadEnv(mode || "development", process.cwd(), '')
-    return {
-        base: env.VITE_APP_URL,
-        plugins: [
-            react(),
-            VitePWAPlugin(env.VITE_APP_URL),
-            nodePolyfills(), // https://www.npmjs.com/package/vite-plugin-node-polyfills
-        ],
-        server: {
-            host: "0.0.0.0"
-        },
-        preview: {
-            host: "0.0.0.0"
-        },
-    } as UserConfig
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode || "development", process.cwd(), "");
+  return {
+    base: env.VITE_APP_URL,
+    plugins: [
+      react(),
+      nodePolyfills(), // https://www.npmjs.com/package/vite-plugin-node-polyfills
+    ],
+    server: {
+      host: "0.0.0.0",
+    },
+    preview: {
+      host: "0.0.0.0",
+    },
+  } as UserConfig;
 });
