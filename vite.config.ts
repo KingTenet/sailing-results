6263790a1fd0eb2fd9eeaf841128316ce6d336c5
Felix Morley <felix@greyparrot.ai>
2026-01-14 08:38:33 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -30,10 +30,8 @@ export default defineConfig(({ mode }: UserConfig) => {
         server: {
             host: "0.0.0.0",
             port: 3000
-            port: 3000
         },
         preview: {
-            host: "0.0.0.0"
             host: "0.0.0.0"
         },
         resolve: {
